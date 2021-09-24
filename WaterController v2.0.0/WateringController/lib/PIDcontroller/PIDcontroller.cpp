30ffd75cd9a60e15e66d8d3324c923c35c2a20c3
Arkadiusz Kruszyński <xxxareckixxx@gmail.com>
2021-09-24 17:53:18 +0200

    Add new methods to PID controller

--- WaterController v2.0.0/WateringController/lib/PIDcontroller/PIDcontroller.cpp
+++ WaterController v2.0.0/WateringController/lib/PIDcontroller/PIDcontroller.cpp
@@ -9,3 +9,55 @@ void PIDcontroller::SetValuePI(int P, int I)
     this->Pcontroller = P;
     this->Icontroller = I;
 }
+
+void PIDcontroller::SetValuePID(int P, int I, int D)
+{
+    this->Pcontroller = P;
+    this->Dcontroller = D;
+    this->Icontroller = I;
+}
+
+int PIDcontroller::PIDwork(int setValue)
+{
+    this->sampleTime = millis();
+    float tempSample = (this->sampleTime - this->sampleTimePriv) / 1000;
+    this->sampleTimePriv = this->sampleTime;
+    this->error = setValue - this->outputValue;
+
+    switch (typeOfController)
+    {
+    case 0:
+        return 0;
+        break;
+
+    case 1:
+        this->outputValue = PUnit();
+        break;
+    case 2:
+        this->outputValue = PIUnit(tempSample);
+        break;
+
+    case 3:
+        this->outputValue = PIDUnit(tempSample);
+        break;
+    }
+
+    return 1;
+}
+
+float PIDcontroller::PUnit()
+{
+    return this->error * this->Pcontroller;
+}
+
+float PIDcontroller::PIUnit(int sample)
+{
+    this->cumIController += this->error * sample;
+    return PUnit() + this->cumIController * this->Icontroller;
+}
+
+float PIDcontroller::PIDUnit(int sample)
+{
+
+    return PUnit() + PIUnit(sample) + (this->error - this->errorPriv) * this->Dcontroller / sample;
+}
