f096a169d7bdff2d6cb61b92887c43fd81d5d805
Arkadiusz Kruszyński <arkadiusz.h.kruszynski@gmail.com>
2021-09-26 23:09:29 +0200

    no message

--- WaterController v2.0.0/WateringController/src/main.cpp
+++ WaterController v2.0.0/WateringController/src/main.cpp
@@ -1,27 +1,31 @@
-#include <Arduino.h>
-#include <WiFi.h>
+#include "PIDcontroller.h"
 #include "WiFiClientSecure.h"
-#include <string.h>
-#include <EEPROM.h>
-#include <WebSocketsClient.h>
+#include <Arduino.h>
 #include <ArduinoJson.h>
 #include <DallasTemperature.h>
-#include "PIDcontroller.h"
+#include <EEPROM.h>
+#include <WebSocketsClient.h>
+#include <WiFi.h>
+#include <string.h>
 
 #define addressID 1
+#define addressScaler 2
 #define baudRate 9600
-#define maskID 5012
-#define serverIP "100.100.100.100"
+#define serverIP "192.168.0.250"
 #define port 80
 #define reconnectInterval 5000
+#define ONE_WIRE_BUS 32
 
 //Define variables
 
 float espID = 0;
 float espRemoteID = 0;
 
-const char *ssid = "LOGIN";
-const char *password = "PASSWORD";
+const char *ssid = "2.4G_echostar_5c82";
+const char *password = "00035022";
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensorTemperature(&oneWire);
 
 WiFiClient client;
 StaticJsonDocument<200> document;
@@ -31,62 +35,61 @@ WebSocketsClient webSocket;
 
 //Define functions
 
-void okMessage(String logTrigger, String info)
-{
-  Serial.println("[OK] + [" + logTrigger + "] " + info);
+void okMessage(String logTrigger, String info) {
+    Serial.println("[OK] + [" + logTrigger + "] " + info);
 }
 
-void infoMessage(String logTrigger, String info)
-{
-  Serial.println("[MESSAGE] + [" + logTrigger + "] " + info);
+void infoMessage(String logTrigger, String info) {
+    Serial.println("[MESSAGE] + [" + logTrigger + "] " + info);
 }
 
-void errorMessage(String logTrigger, String info)
-{
-  Serial.println("[ERROR] + [" + logTrigger + "] " + info);
+void errorMessage(String logTrigger, String info) {
+    Serial.println("[ERROR] + [" + logTrigger + "] " + info);
 }
 
-bool validID(int ID)
-{
-  espID = EEPROM.read(addressID);
-  espID = espID * maskID;
+bool validID(int ID) {
+    espID = EEPROM.read(addressID);
+    espID = espID * EEPROM.read(addressScaler);
 
-  if (espID == ID)
-  {
-    Serial.println();
-    return true;
-  }
+    if (espID == ID) {
+        okMessage("ID VALIDATION", "ID OKAY");
+        return true;
+    }
 
-  return false;
+    errorMessage("ID VALIDATION", "ID WRONG");
+
+    return false;
 }
 
-void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
-{
+float readTemnperature() {
+    sensorTemperature.requestTemperatures();
+    float temperature = sensorTemperature.getTempCByIndex(0);
+    return temperature;
+}
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
 }
 
 //END Define functions
 
-void setup()
-{
-  Serial.begin(baudRate);
-  WiFi.begin(ssid, password);
+void setup() {
+    Serial.begin(baudRate);
+    WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    infoMessage("SETUP", "Waiting...");
-    delay(500);
-  }
+    while (WiFi.status() != WL_CONNECTED) {
+        infoMessage("SETUP", "Waiting...");
+        delay(500);
+    }
 
-  //websocket
-  webSocket.begin(serverIP, port, "/");
-  webSocket.onEvent(webSocketEvent);
-  webSocket.setReconnectInterval(reconnectInterval);
+    //websocket
+    webSocket.begin(serverIP, port, "/");
+    webSocket.onEvent(webSocketEvent);
+    webSocket.setReconnectInterval(reconnectInterval);
 
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 }
 
-void loop()
-{
-  webSocket.loop();
-  // put your main code here, to run repeatedly:
+void loop() {
+    webSocket.loop();
+    // put your main code here, to run repeatedly:
 }
